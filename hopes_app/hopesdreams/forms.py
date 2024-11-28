0453dc8ad5f1ae1fe8ee6d20bfa298aa2ab76a7d
Jeremy Tan <jeremysantostan@gmail.com>
2024-11-28 23:41:06 +0800

    Deleted Hopes_app

--- hopes_app/hopesdreams/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Dreamer
-
-class DreamerForm(ModelForm):
-    class Meta:
-        model = Dreamer
-        fields = ['firstname', 'lastname']
